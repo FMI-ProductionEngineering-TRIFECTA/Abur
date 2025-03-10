7db2c04b8953b32cd1b4d0462549796738cdd239
Giulian617 <buzatugiulian@yahoo.com>
2025-03-10 17:05:55 +0200

    [FEAT] Add user validation

--- src/main/java/ro/unibuc/hello/service/LibraryService.java
+++ src/main/java/ro/unibuc/hello/service/LibraryService.java
@@ -3,8 +3,10 @@ package ro.unibuc.hello.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.http.ResponseEntity;
+import ro.unibuc.hello.data.entity.UserEntity;
 import ro.unibuc.hello.data.repository.LibraryRepository;
 import ro.unibuc.hello.data.repository.UserRepository;
+import ro.unibuc.hello.exception.NotFoundException;
 
 import static org.springframework.http.ResponseEntity.ok;
 import static ro.unibuc.hello.data.entity.UserEntity.Role;
@@ -19,6 +21,9 @@ public class LibraryService {
     protected UserRepository userRepository;
 
     public ResponseEntity<?> getLibraryByCustomerId(String customerId) {
+        UserEntity customer = userRepository.findByIdAndRole(customerId, Role.CUSTOMER);
+        if (customer == null) throw new NotFoundException("No customer found at id %s", customerId);
+        
         return ok(libraryRepository.findGamesByCustomer(userRepository.findByIdAndRole(customerId, Role.CUSTOMER)));
     }
 
