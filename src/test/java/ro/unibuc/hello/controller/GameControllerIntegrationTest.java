5936430747f5cb550677a606b8148d10fc90a601
sebimih13 <sebimih@gmail.com>
2025-04-08 15:55:42 +0300

    [FEAT] Jenkinsfile - Run Integration Tests

--- src/test/java/ro/unibuc/hello/controller/GameControllerIntegrationTest.java
+++ src/test/java/ro/unibuc/hello/controller/GameControllerIntegrationTest.java
@@ -29,7 +29,7 @@ public class GameControllerIntegrationTest extends GenericControllerIntegrationT
 
     @DynamicPropertySource
     private static void setProperties(DynamicPropertyRegistry registry) {
-        final String MONGO_URL = "mongodb://localhost:";
+        final String MONGO_URL = "mongodb://host.docker.internal:";
         final String PORT = String.valueOf(mongoDBContainer.getMappedPort(27017));
 
         registry.add("mongodb.connection.url", () -> MONGO_URL + PORT);
